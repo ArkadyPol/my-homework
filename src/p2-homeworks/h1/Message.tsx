6baa08bc937e8c9f081c59e9fdcf9856478a67f9
Arkady Polhovsky <arkims@inbox.ru>
2021-05-26 18:42:42 +0300

    home task 1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
-    return (
-        <div>
+type MessageType = {
+  avatar: string
+  name: string
+  message: string
+  time: string
+}
 
+function Message(props: MessageType) {
+  return (
+      <div className={s.message}>
+        <div className={s.image}><img src={props.avatar} alt="avatar"/></div>
+        <div className={s.rectangle}>
+          <div className={s.name}>{props.name}</div>
+          <div className={s.messageText}>{props.message} </div>
+          <div className={s.time}>{props.time} </div>
         </div>
-    )
+      </div>
+  )
 }
 
 export default Message
