245f87901a91fbabb1df272611eee3dc7315453e
Arkady Polhovsky <arkims@inbox.ru>
2021-05-26 23:48:52 +0300

    home task 2

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,21 +1,35 @@
 import React from 'react'
+import {AffairPriorityType, AffairType} from './HW2'
+import s from './Affairs.module.css'
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+  // key не нужно типизировать
+  affair: AffairType // need to fix any
+  deleteAffairCallback: (_id: number) => void // need to fix any
+}
+
+type ColorType = 'red' | 'yellow' | 'green'
+type ColorsType = {
+  [key in AffairPriorityType]: ColorType
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
-
-    return (
-        <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
-        </div>
-    )
+  const deleteCallback = () => {
+    props.deleteAffairCallback(props.affair._id)
+  }// need to fix
+  const colors: ColorsType = {
+    high: 'red',
+    middle: 'yellow',
+    low: 'green',
+  }
+  return (
+      <div className={s.affair}>
+        {/*show some text*/}
+        <div>{props.affair.name}</div>
+        <div style={{color: colors[props.affair.priority]}}>{props.affair.priority}</div>
+        <button onClick={deleteCallback}>X</button>
+      </div>
+  )
 }
 
 export default Affair
--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,38 +1,48 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import {AffairType, FilterType} from './HW2'
+import s from './Affairs.module.css'
 
 type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+  data: Array<AffairType>
+  filter: FilterType
+  setFilter: (filter: FilterType) => void
+  deleteAffairCallback: (_id: number) => void
 }
 
 function Affairs(props: AffairsPropsType) {
-    const mappedAffairs = props.data.map((a: AffairType) => (
-        <Affair // should work
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
-            affair={a}
-            deleteAffairCallback={props.deleteAffairCallback}
-        />
-    ))
+  const mappedAffairs = props.data.map((a: AffairType) => (
+      <Affair // should work
+          key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+          affair={a}
+          deleteAffairCallback={props.deleteAffairCallback}
+      />
+  ))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+  const setAll = () => {
+    props.setFilter('all')
+  } // need to fix
+  const setHigh = () => {
+    props.setFilter('high')
+  }
+  const setMiddle = () => {
+    props.setFilter('middle')
+  }
+  const setLow = () => {
+    props.setFilter('low')
+  }
 
-    return (
-        <div>
+  return (
+      <div className={s.affairs}>
 
-            {mappedAffairs}
+        {mappedAffairs}
 
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
-        </div>
-    )
+        <button className={props.filter === 'all' ? s.active : ''} onClick={setAll}>All</button>
+        <button className={props.filter === 'high' ? s.active : ''} onClick={setHigh}>High</button>
+        <button className={props.filter === 'middle' ? s.active : ''} onClick={setMiddle}>Middle</button>
+        <button className={props.filter === 'low' ? s.active : ''} onClick={setLow}>Low</button>
+      </div>
+  )
 }
 
 export default Affairs
