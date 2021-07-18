54950d498dbc2fe8d8d664ed2c6432f3e87b82b8
Arkady Polhovsky <arkims@inbox.ru>
2021-07-18 04:37:42 +0300

    home task 8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,24 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
+import {UserType} from '../HW8';
+
+type ActionType = { type: 'sort', payload: 'up' | 'down' } | { type: 'check', payload: number }
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+  switch (action.type) {
+    case 'sort': {
+      switch (action.payload) {
+        case 'up':
+          return [...state].sort((a, b) => a.name < b.name ? -1 : a.name === b.name ? 0 : 1)
+        case 'down':
+          return [...state].sort((a, b) => a.name > b.name ? -1 : a.name === b.name ? 0 : 1)
+        default:
+          return state
+      }
     }
+    case 'check': {
+      // need to fix
+      return state.filter(u => u.age >= action.payload)
+    }
+    default:
+      return state
+  }
 }
